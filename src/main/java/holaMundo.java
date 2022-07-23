f69e7a0187fa6aa5cf8a16d16dbc723fb03d58a8
anfelole <anfelole11082001@gmail.com>
2022-07-22 20:15:03 -0500

    v1.0.0

---
+++ src/main/java/holaMundo.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author anfel
+ */
+public class holaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int num1 = 7;
+        int num2 = 12;
+        int suma = num1 + num2;
+        System.out.println("El resultado es: " + suma);
+        // TODO code application logic here
+    }
+
+}
